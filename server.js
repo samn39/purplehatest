34ff61683421bcaf1a742cc9fb11d92065f62ede
samn <sanvisethchou@gmail.com>
2022-08-06 23:02:06 -0400

    oops

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use(express.static(path.join(__dirname, "client/build")));
+// app.use(express.static(path.join(__dirname, "client/build")));
 
 if (process.env.NODE_ENV === "production") {
   //server static content
